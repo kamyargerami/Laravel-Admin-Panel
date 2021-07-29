01553dcaae6190574bbdcbb1c3b5e8e3dc5e5435
kamyar gerami <kamyargeramiasl@gmail.com>
2021-07-29 15:23:12 +0430

    rtl emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img src="{{asset('images/logo.png')}}" class="logo" alt="Laravel Logo">
-            @else
-                {{ $slot }}
-            @endif
+            <img src="{{asset('images/logo.png')}}" class="logo" alt="Laravel Logo">
         </a>
     </td>
 </tr>
