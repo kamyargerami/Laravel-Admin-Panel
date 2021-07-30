760a808a3e369ecf1091a3f874dc5cdd596a719b
kamyar gerami <kamyargeramiasl@gmail.com>
2021-07-30 23:57:06 +0430

    update mail route

--- routes/auth.php
+++ routes/auth.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\MobileVerifyController;
 use App\Http\Controllers\Auth\NewPasswordController;
