9b313b6018829818545937785fa9e31b5062d0f5
kamyar gerami <kamyargeramiasl@gmail.com>
2021-08-02 18:35:01 +0430

    add permission seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProductSeeder::class,
             RoleSeeder::class,
+            PermissionSeeder::class
         ]);
     }
 }
