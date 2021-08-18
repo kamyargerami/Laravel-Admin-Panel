d8915100041edeb622da8fd113db6ad34f0527e8
kamyar gerami <kamyargeramiasl@gmail.com>
2021-08-18 17:46:43 +0430

    remove api auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'permission' => \App\Http\Middleware\Permission::class,
-        'api_auth' => \App\Http\Middleware\ApiAuth::class,
         'mobile_verified' => \App\Http\Middleware\MobileVerified::class
     ];
 }
